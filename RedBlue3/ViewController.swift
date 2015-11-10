1e57707d0d2dfb6f12616afbfe80fa90ad3016fe
Cathleen Topness <itunes@topness.net>
2015-11-09 22:14:06 -0500

    Initial Commit

---
+++ RedBlue3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedBlue3
+//
+//  Created by Topness on 11/9/15.
+//  Copyright © 2015 Symmary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
