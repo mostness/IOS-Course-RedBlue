b683bbe8f9968a10594db3cac87e6aee1157e508
Cathleen Topness <itunes@topness.net>
2015-11-10 21:14:03 -0500

    test2

--- RedBlue3/ViewController.swift
+++ RedBlue3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bombBlue: UIImageView!
+    @IBOutlet weak var bombRed: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonHideRed(sender: AnyObject) {
+        bombRed.hidden = true
+    }
+    @IBAction func buttonHideBlue(sender: AnyObject) {
+        bombBlue.hidden = true
+    }
 
 }
 
